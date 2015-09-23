ac19de372fcda574adf5f6a6c83e87e8d2171007
OSM12345 <ian.smelser@gmail.com>
2015-09-23 11:25:37 -0700

    Fade
    Fade

--- 9:23:15/main.lua
+++ 9:23:15/main.lua
@@ -2,10 +2,16 @@
 --Ian Smelser
 --
 
-for i=1,10 do
-	print(i)
-	if (i==5) then
-		print("End")
-		return 0
+local num = 0
+local square = display.newImage("Ch3Square.png", display.contentCenterX,display.contentCenterY)
+local button = display.newImage("Ch3ShowButton.png",display.contentCenterX,display.contentCenterY+300)
+local function fade()
+	if (num==0) then
+		transition.to(square, {time=500, alpha=0})
+		num=1
+	elseif (num==1) then
+		transition.to(square, {time=500, alpha=1})
+		num=0
 	end
 end
+button:addEventListener("tap",fade)
