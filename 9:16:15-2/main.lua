67f000fccf092d989a52d7354943d35052b2560a
OSM12345 <ian.smelser@gmail.com>
2015-09-23 11:09:40 -0700

    Assignment 3
    A:3

--- 9:16:15-2/main.lua
+++ 9:16:15-2/main.lua
@@ -1,21 +1,27 @@
---9/16/15
---Button Counter - Assignment 2
+--9/22/15
+--Button Counter Part 2: Win Condidtion - Assignment 3
 --Ian Smelser
 
---Defing all objects
+--Defing  objects num, button, text
 local num = 0
-local button = display.newImage("button.png",display.contentCenterX,display.contentCenterY+100)
+local button = display.newImage("button.png",display.contentCenterX,display.contentCenterY+200)
 local text = display.newText(num,display.contentCenterX,display.contentCenterY-200,nil,60)
 
---Defing function
+--Count function
 local function count()
-	if num<29 then
+	if num<20 then
+		--Normal count increase and text size grow
 		num=num+1
 		text.text=num
-		text.size=60+(num*4)
+		text.size=60+(num*10)
 	else
+		--Win condidtion. Blue "You Win!" text.
 		text:setFillColor(0,0,0)
-		local endText = display.newText("You can stop now.",display.contentCenterX,display.contentCenterY,nil,60)
+		local endText = display.newText("You Win!",0,0,nil,60)
+		endText.x=display.contentCenterX
+		endText.y=display.contentCenterY
+		endText:setFillColor(0.2,0.3,1)
+		endText.size = endText.size
 	end
 end
 
--- 9:21:15/main.lua
+++ 9:21:15/main.lua
@@ -1,4 +1,12 @@
 --Ian Smelser
 --9/21/15
---
+--Array tables!!!!!
 
+mt = {}
+for i=1,10 do
+	mt[i] = {}
+	for x=1,10 do
+		mt[i][x] = i*x
+		print(mt[i][x])
+	end
+end
